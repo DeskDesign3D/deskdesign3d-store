cdd0ffd94af3c0d9577e043453c5118c5672758c
DeskDesign3D <vanrompayev@gmail.com>
2026-07-02 03:37:29 +0200

    Add Supabase client configuration

--- Config.js
+++ Config.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = https:"//gbrzudblpuhioxblenof.supabase.co/rest/v1/";
 
+const SUPABASE_KEY = "sb_publishable_OeMYh5209JXIyrhvwbA7Rw_U8Q35xxX";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
