bae8e9a805dd2471835f49f6b3bb8fe92ac1f395
DeskDesign3D <vanrompayev@gmail.com>
2026-07-04 00:51:09 +0200

    Fix SUPABASE_URL formatting in config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = https:https:https:"//gbrzudblpuhioxblenof.supabase.co";
+const SUPABASE_URL = "https://gbrzudblpuhioxblenof.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_OeMYh5209JXIyrhvwbA7Rw_U8Q35xxX";
 
