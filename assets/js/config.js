517998d7311a1c82edd27fdfc62da6294160929b
DeskDesign3D <vanrompayev@gmail.com>
2026-07-05 18:14:22 +0200

    Add configuration constants for Supabase and currency

---
+++ assets/js/config.js
@@ -0,0 +1,18 @@
+export const CONFIG = {
+
+    SUPABASE_URL:
+        "YOUR_SUPABASE_URL",
+
+    SUPABASE_ANON_KEY:
+        "YOUR_SUPABASE_ANON_KEY",
+
+    STORAGE_BUCKET:
+        "Product_images",
+
+    CURRENCY:
+        "EUR",
+
+    CURRENCY_LOCALE:
+        "nl-BE"
+
+};
