afe3e92fe5b6154ded3d1612c88e696a9a6ac521
DeskDesign3D <vanrompayev@gmail.com>
2026-07-05 18:16:40 +0200

    Add footer creation function and insert into body

---
+++ assets/js/shared/footer.js
@@ -0,0 +1,25 @@
+export function createFooter(){
+
+return`
+
+<footer>
+
+<p>
+
+© 2026 DeskDesign3D
+
+</p>
+
+</footer>
+
+`;
+
+}
+
+document.body.insertAdjacentHTML(
+
+"beforeend",
+
+createFooter()
+
+);
