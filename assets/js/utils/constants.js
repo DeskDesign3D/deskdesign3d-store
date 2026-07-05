962a29a2d87e7fcef9dd6ac5cbf114263e4838cd
DeskDesign3D <vanrompayev@gmail.com>
2026-07-05 18:15:10 +0200

    Add constants for routes and user roles

---
+++ assets/js/utils/constants.js
@@ -0,0 +1,29 @@
+export const ROUTES = {
+
+    HOME:"index.html",
+
+    PRODUCTS:"products.html",
+
+    PRODUCT:"product.html",
+
+    CART:"cart.html",
+
+    CHECKOUT:"checkout.html",
+
+    LOGIN:"login.html",
+
+    ACCOUNT:"account.html",
+
+    ORDERS:"orders.html",
+
+    ADMIN:"admin.html"
+
+};
+
+export const USER_ROLES={
+
+    CUSTOMER:"customer",
+
+    ADMIN:"admin"
+
+};
