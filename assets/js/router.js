591cf07605980dfedcb0255949b2269505736e97
DeskDesign3D <vanrompayev@gmail.com>
2026-07-05 18:16:03 +0200

    Add router functionality for page logging

---
+++ assets/js/router.js
@@ -0,0 +1,59 @@
+const page=document.body.dataset.page;
+
+switch(page){
+
+    case "home":
+
+        console.log("Home");
+
+    break;
+
+    case "products":
+
+        console.log("Products");
+
+    break;
+
+    case "product":
+
+        console.log("Product");
+
+    break;
+
+    case "cart":
+
+        console.log("Cart");
+
+    break;
+
+    case "checkout":
+
+        console.log("Checkout");
+
+    break;
+
+    case "login":
+
+        console.log("Login");
+
+    break;
+
+    case "account":
+
+        console.log("Account");
+
+    break;
+
+    case "orders":
+
+        console.log("Orders");
+
+    break;
+
+    case "admin":
+
+        console.log("Admin");
+
+    break;
+
+}
